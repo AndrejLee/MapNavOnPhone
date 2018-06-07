c8c217a962fb80cdfd6cfc66f41a912829cb003c
HanLD <hanld@onetech.vn>
2018-06-07 23:53:31 +0700

    for backup

---
+++ Assets/Client.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Client : MonoBehaviour
+{
+
+    private Socket _clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+    private byte[] _recieveBuffer = new byte[8142];
+    private const String COORDINATE_KEYWORD = "0001:";
+    public static float XReceived = 0;
+    public static float YReceived = 0;
+    private bool isSocketReady = false;
+
+    public void SetupServer()
+    {
+        try
+        {
+            _clientSocket.Connect(new IPEndPoint(IPAddress.Parse("192.168.1.10") , 1234));
+            _clientSocket.BeginReceive(_recieveBuffer, 0, _recieveBuffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), null);
+            isSocketReady = true;
+        }
+        catch (SocketException ex)
+        {
+            Debug.Log(ex.Message);
+        }
+
+        
+
+    }
+
+    public void CloseSocket()
+    {
+        if (!isSocketReady)
+            return;
+        try
+        {
+            _clientSocket.Close();
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+
+    }
+
+    private void ReceiveCallback(IAsyncResult AR)
+    {
+        //Check how much bytes are recieved and call EndRecieve to finalize handshake
+        int recieved = _clientSocket.EndReceive(AR);
+
+        if (recieved <= 0)
+            return;
+
+        //Copy the recieved data into new buffer , to avoid null bytes
+        byte[] recData = new byte[recieved];
+        Buffer.BlockCopy(_recieveBuffer, 0, recData, 0, recieved);
+
+        //Process data here the way you want , all your bytes will be stored in recData
+        string Receiver = Encoding.ASCII.GetString(recData);
+        string[] tokens = Receiver.Split('@');
+
+        string keyWord = tokens[0].Substring(0, 5);
+
+        switch (keyWord)
+        {
+            case COORDINATE_KEYWORD:
+                extractCoordination(tokens[0]);
+                break;
+        }
+
+        //Start receiving again
+        _clientSocket.BeginReceive(_recieveBuffer, 0, _recieveBuffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), null);
+    }
+
+    private void extractCoordination(String buffer)
+    {   
+        String data = buffer.Substring(5, buffer.Length - 6);
+        String[] res = data.Split('|');
+        XReceived = float.Parse(res[0]);
+        YReceived = float.Parse(res[1]);
+        MoveCameraController.isLoaded = true;
+    }
+
+    public static Vector2 getReceivedCoord()
+    {
+        return new Vector2(XReceived, YReceived);
+    }
+
+    private void SendData(byte[] data)
+    {
+        SocketAsyncEventArgs socketAsyncData = new SocketAsyncEventArgs();
+        socketAsyncData.SetBuffer(data, 0, data.Length);
+        _clientSocket.SendAsync(socketAsyncData);
+    }
+}
---
+++ Assets/MAPNAV/Scripts/MoveCameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveCameraController : MonoBehaviour {
+
+    public static bool isLoaded = false;
+    private new GameObject camera;
+    private const int frame = 60;
+    private int i = 0;
+    private int move = 0;
+    public Text text;
+    // Use this for initialization
+    void Start () {
+        camera = GameObject.Find("Camera");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (isLoaded)
+        {
+            Vector2 receivedData = Client.getReceivedCoord();
+            text.text = receivedData.x + "\t" + receivedData.y;
+            MoveCamera(receivedData.x, receivedData.y, -12f);
+        }
+	}
+
+    private void MoveCamera(float x, float y, float z)
+    {
+        Vector3 newPos = new Vector3(x, y, z);
+        //camera.transform.localPosition = Vector3.Lerp(camera.transform.localPosition, newPos, Time.deltaTime * 10f);
+        camera.transform.localPosition = newPos;
+    }
+}
